7412156c5a158f48f6f86db92c067435bc1120f2
hiim207 <20jin_lee@naver.com>
2023-12-18 00:05:16 +0900

    OAuth 네이버 카카오

--- src/main/java/com/ansim/service/OAuth2UserDetailsServiceImpl.java
+++ src/main/java/com/ansim/service/OAuth2UserDetailsServiceImpl.java
@@ -39,8 +39,26 @@ public class OAuth2UserDetailsServiceImpl extends DefaultOAuth2UserService{
 		OAuth2User oAuth2User = super.loadUser(userRequest);
 		
 		String provider = userRequest.getClientRegistration().getRegistrationId();
-		String providerId = oAuth2User.getAttribute("sub");
-		String user_id = oAuth2User.getAttribute("email");
+//		String providerId = oAuth2User.getAttribute("sub");
+//		String user_id = oAuth2User.getAttribute("email");
+		String providerId = "";
+		String user_id = "";
+
+		if (provider.equals("naver")) {  // 네이버 로그인인 경우
+			Map<String, Object> attributes = oAuth2User.getAttributes();
+			Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+			providerId = response.get("id").toString();
+			user_id = (String) response.get("email");
+		} else if (provider.equals("google")) {  // 구글 로그인인 경우
+			providerId = oAuth2User.getAttribute("sub");
+			user_id = oAuth2User.getAttribute("email");
+		} else if (provider.equals("kakao")) { //카카오 로그인인 경우
+			Map<String, Object> attributes = oAuth2User.getAttributes();
+			Map<String, Object> response = (Map<String, Object>) attributes.get("kakao_account");
+			System.out.println(response);
+			providerId = response.get("profile").toString();
+			user_id = response.get("email").toString();
+		}
 		
 		log.info("provider = {}", provider);
 		log.info("providerId = {}", providerId);
@@ -50,7 +68,7 @@ public class OAuth2UserDetailsServiceImpl extends DefaultOAuth2UserService{
 			log.info(k + ":" + v);
 		});
 
-		MemberDTO member = saveSocialMember(user_id);
+		MemberDTO member = saveSocialMember(user_id, provider);
 		
 		//Role 값 읽어 들임...
 		List<SimpleGrantedAuthority> grantedAuthorities = new ArrayList<>();
@@ -66,12 +84,13 @@ public class OAuth2UserDetailsServiceImpl extends DefaultOAuth2UserService{
 		session.setAttribute("user_id", member.getUser_id());
 		session.setAttribute("user_nm", member.getUser_nm());
 		session.setAttribute("role", member.getRole());
-		
+		session.setAttribute("fromSocial","Y");
+
 		return memberOAuth2DTO;		
 		
 	}
 	
-	private MemberDTO saveSocialMember(String user_id) throws Exception {
+	private MemberDTO saveSocialMember(String user_id, String provider) throws Exception {
 		
 		//구글 회원 계정으로 로그인 한 회원의 경우 사이트 운영에 필요한 최소한의 정보를 
 		//가공해서 tbl_member에 입력해야 함.
@@ -81,10 +100,19 @@ public class OAuth2UserDetailsServiceImpl extends DefaultOAuth2UserService{
 		if(result.isPresent()) {
 			return result.get();
 		}
+
+		String username = "";
+		if (provider.equals("naver")) {
+			username = "네이버회원";
+		} else if (provider.equals("google")) {
+			username = "구글회원";
+		} else if (provider.equals("kakao")) {
+			username = "카카오회원";
+		}
 		
 		MemberDTO member = MemberDTO.builder()
 										.user_id(user_id)
-										.user_nm("구글회원")
+										.user_nm(username)
 										.password(pwdEncoder.encode("12345"))
 										.role("USER")
 										.regdate(LocalDate.now())
